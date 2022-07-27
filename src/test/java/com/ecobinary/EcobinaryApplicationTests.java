7edcd65d4b48ddbc0e24bf46ea6b48cbc919412b
Elliott03 <elliottjosephphillips@gmail.com>
2022-07-27 08:28:02 -0400

    Initial Commit

---
+++ src/test/java/com/ecobinary/EcobinaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecobinary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcobinaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
